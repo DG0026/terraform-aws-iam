591edfda3df91292c77968a0fce4423cb786dc69
DG0026 <93153436+DG0026@users.noreply.github.com>
2022-08-12 09:54:02 -0400

    Update iamroles.tf
    add region

--- iamroles.tf
+++ iamroles.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_iam_role" "ec2_s3_access_role" {
   name               = "s3-role"
   assume_role_policy = "${file("assumerolepolicy.json")}"
