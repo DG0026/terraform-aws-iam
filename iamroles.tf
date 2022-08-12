d93955bbf45a7f2ffdf410ae2fd382300fccede3
DG0026 <93153436+DG0026@users.noreply.github.com>
2022-08-12 09:55:33 -0400

    Update iamroles.tf

--- iamroles.tf
+++ iamroles.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_iam_role" "ec2_s3_access_role" {
   name               = "s3-role"
   assume_role_policy = "${file("assumerolepolicy.json")}"
